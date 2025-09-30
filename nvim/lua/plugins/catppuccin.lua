1b8249e4fb72b0a94ba8f39f07157e88431ab853
etcetera <hubertetcetera@gmail.com>
2025-09-30 15:27:15 +0530

    feat(nvim): update default theme to catppuccin

---
+++ nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    priority = 1000,
+    opts = { flavour = "mocha", transparent_background = false },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = { colorscheme = "catppuccin" },
+  },
+}
